397a817db67b78eda3d894d986acde58fb11c270
sampath9dasari@gmail.com <sampath9dasari@outlook.com>
2021-03-15 12:56:33 -0400

    Refactor lib

---
+++ model/advattack.py
@@ -0,0 +1,11 @@
+from art.attacks.evasion import ProjectedGradientDescent
+from art.estimators.object_detection import PyTorchFasterRCNN
+
+def gen_adv_model(model):
+    detector = PyTorchFasterRCNN(model=model, clip_values=(0, 1), preprocessing=None)
+    return detector
+
+def gen_adv_attack(model):
+    attack = ProjectedGradientDescent(model, eps=0.01, eps_step=0.01, max_iter=2, verbose=True)
+    return attack
+
